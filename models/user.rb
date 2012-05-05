8720ecf4cd14584ffbb80fa1c293b2f6feb0a8ea
Patrick Arlt <patrick.arlt@gmail.com>
2012-05-05 11:41:06 -0700

    fix syntax error in user

--- models/user.rb
+++ models/user.rb
@@ -20,35 +20,34 @@ class User
   field :geoloqi_user_id, type: String
   field :geoloqi_access_token, type: String
   
-  has_many :alerts
-  has_many :penalties
+  #has_many :alerts
+  #has_many :penalties
   
-  def self.preform user_id task
-    self.all_in.each do |user|
-      ResqueScheduler.schedule({
-        "every"=> "30s",
-        "class"=> "User",
-        "queue"=> "user",
-        "args"=> "test",
-        "description"=> "Ask the user class to queue jobs"
-      })
+  #def self.preform(user_id, task)
+   
+    #self.all_in.each do |user|
+      #ResqueScheduler.schedule({
+      #  "every"=> "30s",
+      #  "class"=> "User",
+      #  "queue"=> "user",
+      #  "args"=> "test",
+      #  "description"=> "Ask the user class to queue jobs"
+      #})
 
-      puts user.inspect
-    end
-  end
+      #puts user.inspect
+    #end
+
+  #end
 
   # Update a users location (using at&t)
-  def update_location
-  end
+  #def update_location
+  #end
 
   # Update at&t refresh token
-  def update_refresh_token
-  end
+  #def update_refresh_token
+  #end
 
   # Update alerts
-  def update_alerts
-  end
-
-
-
+  #def update_alerts
+  #end
 end
