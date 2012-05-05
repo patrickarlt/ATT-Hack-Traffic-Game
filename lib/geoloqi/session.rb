6ef108aa4a1b804552764c791fae530b0bc1914f
Patrick Arlt <patrick.arlt@gmail.com>
2012-05-05 13:10:37 -0700

    fix geoloqi applciation methods

--- lib/geoloqi/session.rb
+++ lib/geoloqi/session.rb
@@ -1,11 +1,11 @@
 module Geoloqi
   class Session
     def application_get(path, opts={})
-      get path, opts, {"Authorization" => "Basic " + Base64.strict_encode64(@config.client_id + ":" + @config.client_secret)}
+      get path, opts, {"Authorization" => "Basic " + Base64.strict_encode64(ENV["GEOLOQI_CLIENT_ID"] + ":" + ENV["GEOLOQI_CLIENT_SECRET"])}
     end
 
     def application_post(path, opts={})
-      post path, opts, {"Authorization" => "Basic " + Base64.strict_encode64(@config.client_id + ":" + @config.client_secret)}
+      post path, opts, {"Authorization" => "Basic " + Base64.strict_encode64(ENV["GEOLOQI_CLIENT_ID"] + ":" + ENV["GEOLOQI_CLIENT_SECRET"])}
     end
   end
 end
