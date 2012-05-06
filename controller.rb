8b0adfee9314a6051030c27f6fe9e234b56f2450
Patrick Arlt <patrick.arlt@gmail.com>
2012-05-05 19:19:02 -0700

    integration

--- controller.rb
+++ controller.rb
@@ -33,13 +33,13 @@
   end
 
   get '/callback/alert' do
-    "data": {
+    data = {
       "place":{
       "place_id":"200",
        "name":"Palo Alto",
        "latitude":"37.441898",
        "longitude":"-122.141899",
-       "extra":{
+       "extra": {
           "description":"Car accident ahead!",
           "mq_id" : "200",
           "traffic_jam_url": "hollow-fog-8448.herokuapp.com/og/traffic_jam_200.html"
